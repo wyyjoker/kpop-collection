911a41f6dd6a9514d7b0f440d54e38deaff8d985
wyyjoker <140365247+wyyjoker@users.noreply.github.com>
2026-09-13 20:07:02 +0800

    feat: load V0.5A scrapbook layer

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-for (const href of ['/v0.1b.css', '/v0.2.css', '/v0.3.css', '/v0.4.css']) {
+for (const href of ['/v0.1b.css', '/v0.2.css', '/v0.3.css', '/v0.4.css', '/v0.5a.css']) {
   const link = document.createElement('link');
   link.rel = 'stylesheet';
   link.href = href;
@@ -10,6 +10,7 @@ import('/app-v0.1b.js')
   .then(() => import('/app-v0.3.js'))
   .then(() => import('/app-v0.3-bridge.js'))
   .then(() => import('/app-v0.4.js'))
+  .then(() => import('/app-v0.5a.js'))
   .catch((error) => {
     console.error('Failed to load K-pop Collection client:', error);
   });
